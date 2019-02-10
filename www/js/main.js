36934936aac1c9147f3fe660e724505c7e0f9385
aczzdx <aczzdx@gmail.com>
2019-02-10 11:12:10 -0800

    fix typos

--- www/js/main.js
+++ www/js/main.js
@@ -1,17 +1,18 @@
-$(document).ready(
-    function() {
-        var user = firebase.auth().currentUser;        
-        user.getIdToken(true).then(function(idToken) {
-            var getCredit = firebase.functions().httpsCallable('getCredit');
-            addUser().then(function(result){ 
-              console.log(`get credit = ${result.credit}`);
-              $(".credit-box").update(result.credit);
-              window.location = "/main_interface.html.html";
-            }).catch(function(error) {
-              var code = error.code;
-              var message = error.message;
-              var details = error.details;
-              console.log(message);
-            });
-    }
-)});
+
+function getCreditScore() {
+    var user = firebase.auth().currentUser;
+    user.getIdToken(true).then(function(idToken) {
+        var getCredit = firebase.functions().httpsCallable('getCredit');
+        getCredit().then(function(result){ 
+          console.log(`get credit = ${result.credit}`);
+          $(".credit-box").update(result.credit);
+          window.location = "/main_interface.html.html";
+        }).catch(function(error) {
+          var code = error.code;
+          var message = error.message;
+          var details = error.details;
+          console.log(message);
+        }); });
+}
+
+$(document).ready(getCreditScore);
