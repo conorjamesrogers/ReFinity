8d2e4141fe0ae7db3679ef8651f0290049a8d04a
aczzdx <aczzdx@gmail.com>
2019-02-10 10:38:11 -0800

    add credit-update function

---
+++ www/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(
+    function() {
+        var user = firebase.auth().currentUser;        
+        user.getIdToken(true).then(function(idToken) {
+            var getCredit = firebase.functions().httpsCallable('getCredit');
+            addUser().then(function(result){ 
+              console.log(`get credit = ${result.credit}`);
+              $(".credit-box").update(result.credit);
+              window.location = "/main_interface.html.html";
+            }).catch(function(error) {
+              var code = error.code;
+              var message = error.message;
+              var details = error.details;
+              console.log(message);
+            });
+    }
+)});
