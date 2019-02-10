591a0df66910ab4a7d0ed22ed2f74c69445454a4
aczzdx <aczzdx@gmail.com>
2019-02-10 02:15:51 -0800

    add signup function

--- www/js/script.js
+++ www/js/script.js
@@ -1,5 +1,14 @@
 // assure created password matches
-import * as firebase from '/firebase';
+
+var config = {
+  apiKey: "AIzaSyB-hOBjpeE7GHViS7HkO8ZuqEnUSK9x8Ks",
+  authDomain: "refinity-75d88.firebaseapp.com",
+  databaseURL: "https://refinity-75d88.firebaseio.com",
+  projectId: "refinity-75d88",
+  storageBucket: "refinity-75d88.appspot.com",
+  messagingSenderId: "955524219940"
+};
+firebase.initializeApp(config);
 
 var password = document.getElementById("password"), rep_password = document.getElementById("rep-password");
 
@@ -15,14 +24,38 @@ password.onchange = validatePassword;
 rep_password.onkeyup = validatePassword;
 
 
-
-
 // open form to create new account
 $(document).ready(function() {
-    $('.create').click(function () {
-        console.log('ahoj.');
-        $('.log-in').addClass('hide');
-        $('.create-acc').removeClass('hide');
-        $('.create').addClass('hide');
-    });
+  $('.create').click(function () {
+    console.log('ahoj.');
+    $('.log-in').addClass('hide');
+    $('.create-acc').removeClass('hide');
+    $('.create').addClass('hide');
+  });
+
+  $('.create-btn').click(function () {
+    if (!validatePassword()) {
+      return;
+    }
+    firebase.auth().createUserWithEmailAndPassword(
+      $('#email-sign').val(),
+      $('#password').val()).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if (errorCode == 'auth/email-already-in-use') {
+          alert('The email has been used to register, try login?');
+        }
+        else if (errorCode == 'auth/invalid-email') {
+          alert('The email address is invalid.');
+        }
+        else {
+          alert(errorMessage);
+        }
+
+        console.log(error);
+      });
+
+
+  });
 });
