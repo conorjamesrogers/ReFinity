b71fa98ecc032a43170da8e90469406308d862fb
aczzdx <aczzdx@gmail.com>
2019-02-10 11:17:53 -0800

    add firebase code inside main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,14 @@
+var config = {
+  apiKey: "AIzaSyB-hOBjpeE7GHViS7HkO8ZuqEnUSK9x8Ks",
+  authDomain: "refinity-75d88.firebaseapp.com",
+  databaseURL: "https://refinity-75d88.firebaseio.com",
+  projectId: "refinity-75d88",
+  storageBucket: "refinity-75d88.appspot.com",
+  messagingSenderId: "955524219940"
+};
+firebase.initializeApp(config);
+
+var functions = firebase.functions();
 
 function getCreditScore() {
     var user = firebase.auth().currentUser;
